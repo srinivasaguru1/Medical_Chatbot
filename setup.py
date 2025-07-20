56397d359bd8bd2f4638bd84f6610bb172bc7793
srinivasaguru1 <srinivasaguru301@gmail.com>
2025-07-20 17:39:08 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Aguru Srinivas",
+    author_email="srinivasaguru301@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
